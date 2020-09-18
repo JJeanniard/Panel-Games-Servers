82ded7d4c18a6dfbea3964132111a618c1846a93
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-09-18 06:12:32 +0200

    update user à la conformiter de la doc

--- src/src/Entity/User.php
+++ src/src/Entity/User.php
@@ -2,19 +2,19 @@
 
 namespace App\Entity;
 
-use App\Repository\UserPseudoRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass=UserPseudoRepository::class)
+ * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class UserPseudo implements UserInterface
+class User implements UserInterface
 {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="smallint")
      */
     private $id;
 
@@ -33,6 +33,21 @@ class UserPseudo implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $user_lastname;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $user_fistname;
+
+    /**
+     * @ORM\Column(type="string", length=50, unique=true)
+     */
+    private $user_pseudo;
 
     public function getId(): ?int
     {
@@ -58,7 +73,7 @@ class UserPseudo implements UserInterface
      */
     public function getUsername(): string
     {
-        return (string) $this->user_email;
+        return (string) $this->email;
     }
 
     /**
@@ -110,5 +125,41 @@ class UserPseudo implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getUserLastname(): ?string
+    {
+        return $this->user_lastname;
+    }
+
+    public function setUserLastname(?string $user_lastname): self
+    {
+        $this->user_lastname = $user_lastname;
+
+        return $this;
+    }
+
+    public function getUserFistname(): ?string
+    {
+        return $this->user_fistname;
+    }
+
+    public function setUserFistname(string $user_fistname): self
+    {
+        $this->user_fistname = $user_fistname;
+
+        return $this;
+    }
+
+    public function getUserPseudo(): ?string
+    {
+        return $this->user_pseudo;
+    }
+
+    public function setUserPseudo(string $user_pseudo): self
+    {
+        $this->user_pseudo = $user_pseudo;
+
+        return $this;
     }
 }
--- src/src/Repository/UserRepository.php
+++ src/src/Repository/UserRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\UserPseudo;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
@@ -10,16 +10,16 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @method UserPseudo|null find($id, $lockMode = null, $lockVersion = null)
- * @method UserPseudo|null findOneBy(array $criteria, array $orderBy = null)
- * @method UserPseudo[]    findAll()
- * @method UserPseudo[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserPseudoRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, UserPseudo::class);
+        parent::__construct($registry, User::class);
     }
 
     /**
@@ -27,7 +27,7 @@ class UserPseudoRepository extends ServiceEntityRepository implements PasswordUp
      */
     public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
     {
-        if (!$user instanceof UserPseudo) {
+        if (!$user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
         }
 
@@ -37,7 +37,7 @@ class UserPseudoRepository extends ServiceEntityRepository implements PasswordUp
     }
 
     // /**
-    //  * @return UserPseudo[] Returns an array of UserPseudo objects
+    //  * @return User[] Returns an array of User objects
     //  */
     /*
     public function findByExampleField($value)
@@ -54,7 +54,7 @@ class UserPseudoRepository extends ServiceEntityRepository implements PasswordUp
     */
 
     /*
-    public function findOneBySomeField($value): ?UserPseudo
+    public function findOneBySomeField($value): ?User
     {
         return $this->createQueryBuilder('u')
             ->andWhere('u.exampleField = :val')
