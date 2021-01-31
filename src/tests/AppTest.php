90079d79e9b86a43d7da25b59523d83689344be9
Jonasbadboys <jonathanjeanniard@sfr.fr>
2021-01-31 11:47:00 +0100

    test unitaire & fixture

--- src/tests/AppTest.php
+++ src/tests/AppTest.php
@@ -1,2 +1,11 @@
 <?php
 namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+    public function testTestsAreWorking(){
+        $this->assertEquals(3, 1+1);//Test le calcul avec sur le résultat qu'on attend
+    }
+}
